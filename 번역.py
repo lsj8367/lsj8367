3bb3abb9a3692da400c537f87ad59cf360991c88
이승재 <lsj8367@naver.com>
2020-12-24 17:13:13 +0900

    Add files via upload

---
+++ 번역.py
@@ -0,0 +1,31 @@
+'''
+한국어(ko)-영어(en), 한국어(ko)-일본어(ja), 한국어(ko)-중국어 간체(zh-CN), 한국어(ko)-중국어 번체(zh-TW), 
+한국어(ko)-스페인어(es), 한국어(ko)-프랑스어(fr), 한국어(ko)-러시아어(ru), 한국어(ko)-베트남어(vi), 한국어(ko)-태국어(th), 
+한국어(ko)-인도네시아어(id), 한국어(ko)-독일어(de), 한국어(ko)-이탈리아어(it), 중국어 간체(zh-CN) - 중국어 번체(zh-TW), 중국어 간체(zh-CN) - 일본어(ja), 
+중국어 번체(zh-TW) - 일본어(ja), 영어(en)-일본어(ja), 영어(en)-중국어 간체(zh-CN), 영어(en)-중국어 번체(zh-TW), 영어(en)-프랑스어(fr)를 지원합니다.
+* 처리한도 : 10,000글자/일
+'''
+import os
+import sys
+import urllib.request
+import json
+client_id = "ysnDNp6NQiZonIqc5q5f" # 개발자센터에서 발급받은 Client ID 값
+client_secret = "OyW27rpluA" # 개발자센터에서 발급받은 Client Secret 값
+encText = urllib.parse.quote("어케번역하누")
+
+data = "source=ko&target=en&text=" + encText
+url = "https://openapi.naver.com/v1/papago/n2mt"
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+
+response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read()
+    # print(response_body.decode('utf-8'))
+    #json 형 변환
+    res = json.loads(response_body.decode('utf-8'))
+    print(res['message']['result']['translatedText'])
+else:
+    print("Error Code:" + rescode)
